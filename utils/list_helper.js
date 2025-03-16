08e4c065afef70e012c6792aedc47c8037a16a34
harindu-pe <harindu.pe@gmail.com>
2025-03-16 13:38:25 +0530

    exercise 4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,17 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  let sum = 0;
+
+  blogs.map((blog) => {
+    sum += blog.likes;
+  });
+
+  return sum;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
