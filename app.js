ede35d84045e5449ae9b75ac965972d8e940b9cf
harindu-pe <harindu.pe@gmail.com>
2025-05-13 16:06:36 +0530

    exercise 4.20 done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
@@ -13,27 +14,12 @@ mongoose.connect(config.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 
+app.use(middleware.tokenExtractor);
+
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
 
-const errorHandler = (error, request, response, next) => {
-  if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  } else if (
-    error.name === "MongoServerError" &&
-    error.message.includes("E11000 duplicate key error")
-  ) {
-    return response
-      .status(400)
-      .json({ error: "expected `username` to be unique" });
-  } else if (error.name === "JsonWebTokenError") {
-    return response.status(401).json({ error: "token invalid" });
-  }
-
-  next(error);
-};
-
-app.use(errorHandler);
+app.use(middleware.errorHandler);
 
 module.exports = app;
---
+++ utils/middleware.js
@@ -0,0 +1,32 @@
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (
+    error.name === "MongoServerError" &&
+    error.message.includes("E11000 duplicate key error")
+  ) {
+    return response
+      .status(400)
+      .json({ error: "expected `username` to be unique" });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "token invalid" });
+  }
+
+  next(error);
+};
+
+const tokenExtractor = (request, response, next) => {
+  // code that extracts the token
+  const authorization = request.get("authorization");
+
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+  }
+
+  next();
+};
+
+module.exports = {
+  errorHandler,
+  tokenExtractor,
+};
