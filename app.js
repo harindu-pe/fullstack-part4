ee9527044c2a9682adfda14d3896cb03ce0586ed
harindu-pe <harindu.pe@gmail.com>
2025-05-13 13:48:33 +0530

    exercise 4.18 done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === "ValidationError") {
